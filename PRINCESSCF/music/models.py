8ddd977078e88bb125d5faba019f6d024f671bda
ningpop <craft1933@naver.com>
2021-11-18 23:19:56 +0900

    음악 모델 구현 및 music 앱 추가

--- PRINCESSCF/music/models.py
+++ PRINCESSCF/music/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Music(models.Model):
     artist = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
+    is_not_played = models.BooleanField(default=True)
 
     def __str__(self):
         return self.artist + " - " + self.title
